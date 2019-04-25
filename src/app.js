ca6d0b116d2a967e55e1a3992794643b3b6278da
feteneri <fe.teneri@gmail.com>
2019-04-25 21:50:15 +0500

    line1 added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import "./assets/styles/reset.css";
 import header from "./components/header/header.js";
+import line1 from "./components/line1/line1.js";
 
 
 document.querySelector("#header").innerHTML = header();
+document.querySelector("#line1").innerHTML = line1();
---
+++ src/components/line1/line1.js
@@ -0,0 +1,4 @@
+import template from "./line1.pug";
+import "./line1.css";
+
+export default template;

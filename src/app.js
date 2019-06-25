51cb673ab58013f61fd3c1bc9ffc270aef8706ff
feteneri <fe.teneri@gmail.com>
2019-06-25 17:21:04 +0500

    fixes in line1, line2 added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import "./assets/styles/reset.css";
 import header from "./components/header/header.js";
 import line1 from "./components/line1/line1.js";
+import line2 from "./components/line2/line2.js";
 
 
 document.querySelector("#header").innerHTML = header();
 document.querySelector("#line1").innerHTML = line1();
+document.querySelector("#line2").innerHTML = line2();
